5c7350855403b845dce59268997a1d4543f02193
Gary Nuñez <yragi.nunez@gmail.com>
2021-06-08 22:33:43 -0500

    started search componenet

---
+++ src/search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
+import Book from './book';
+
+class Search extends Component {
+    state = {
+        query: '',
+        result: [],
+    };
+    render() {
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <button
+                        className="close-search"
+                        onClick={() => this.setState({ showSearchPage: false })}
+                    >
+                        Close
+                    </button>
+                    <div className="search-books-input-wrapper">
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                        />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Search;
